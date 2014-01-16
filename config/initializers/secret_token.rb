4739265127a372608cd038eb224f4bf738d5849c
Nejuf <nejuf1@gmail.com>
2014-01-15 21:05:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BunnyRescue::Application.config.secret_key_base = 'a4bee6b53749e2af2e91fede1daf607f2e231ec881a196a0adc6a2e63669826b70b73ab906b48e52a99f655373d3b9ce6971a0d84f99a3d0cca8cbf6a53a9880'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BunnyRescue::Application.config.session_store :cookie_store, key: '_BunnyRescue_session'
